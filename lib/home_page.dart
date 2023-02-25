20185970b5acf2629018cef85d36f3cf3d69e287
nicholasvp <nicholasvpinheiro@gmail.com>
2023-02-25 15:28:11 -0300

    mask

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -22,13 +23,26 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Image.network(
-            'https://images.unsplash.com/photo-1655635949348-953b0e3c140a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1054&q=80',
-            fit: BoxFit.cover,
-          ),
-        ),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                    'https://img.freepik.com/vetores-gratis/fundo-de-formas-3d-realistas_52683-75853.jpg?w=996&t=st=1677349153~exp=1677349753~hmac=3320cf40f877979a12861fb7bfd641d74779b45d7d02c72936bc402975003f9f',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
+            )),
       ),
     );
   }
